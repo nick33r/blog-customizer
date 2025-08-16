f83990abe3c516fe268488cc55a07f81153a93c5
nick33r <andrys33rus@gmail.com>
2025-08-16 17:10:03 +0300

    feat(review): update form component

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,20 +1,51 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import React, { useState, useRef } from 'react';
+import { useOutsideClickClose } from './hooks/useOutsideClickClose';
 
 import styles from './ArticleParamsForm.module.scss';
+import clsx from 'clsx';
 
-export const ArticleParamsForm = () => {
+type FormProps = {
+	fields: React.ReactNode[];
+	onSubmit: () => void;
+	onReset: () => void;
+	opened?: boolean;
+}
+
+export const ArticleParamsForm = ({ fields, onSubmit, onReset, opened = false }: FormProps) => {
+	const [isOpen, setIsOpen] = useState(opened);
+	const rootRef = useRef<HTMLDivElement>(null);
+
+	useOutsideClickClose({
+		isOpen,
+		rootRef,
+		onClose: () => setIsOpen(false),
+	});
+
+	const toggleOpen = () => {
+		setIsOpen(!isOpen);
+	};
+
+	const handleSubmit = (e: React.FormEvent) => {
+		e.preventDefault();
+		onSubmit();
+	};
+
 	return (
-		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+		<div ref={rootRef}>
+			<ArrowButton isOpen={isOpen} onClick={toggleOpen} />
+			<aside className={clsx(styles.container, { [styles.container_open]: isOpen })}>
+				<form className={styles.form} onSubmit={handleSubmit}>
+					<div className={styles.topContainer}>
+						{fields.map((input) => input)}
+					</div>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
+						<Button title='Сбросить' htmlType='reset' type='clear' onClick={onReset} />
 						<Button title='Применить' htmlType='submit' type='apply' />
 					</div>
 				</form>
 			</aside>
-		</>
+		</div>
 	);
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,49 @@
 import { createRoot } from 'react-dom/client';
-import { StrictMode, CSSProperties } from 'react';
+import { useState, StrictMode, CSSProperties } from 'react';
 import clsx from 'clsx';
 
 import { Article } from './components/article/Article';
 import { ArticleParamsForm } from './components/article-params-form/ArticleParamsForm';
-import { defaultArticleState } from './constants/articleProps';
+import {
+	// объекты с данными для пропсов инпутов
+	fontFamilyOptions,
+	fontSizeOptions,
+	fontColors,
+	backgroundColors,
+	contentWidthArr,
+
+	// дефолтные значения инпутов
+	defaultArticleState
+} from './constants/articleProps';
 
 import './styles/index.scss';
 import styles from './styles/index.module.scss';
+import { Select } from './ui/select';
+import { RadioGroup } from './ui/radio-group';
+import { Separator } from './ui/separator';
 
 const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 
 const App = () => {
+	const [fontFamily, setFontFamily] = useState(defaultArticleState.fontFamilyOption);
+	const [fontSize, setFontSize] = useState(defaultArticleState.fontSizeOption);
+	const [fontColor, setFontColor] = useState(defaultArticleState.fontColor);
+	const [backgroundColor, setBackgroundColor] = useState(defaultArticleState.backgroundColor);
+	const [contentWidth, setContentWidth] = useState(defaultArticleState.contentWidth);
+
+	const handleFormSubmit = () => {
+		console.log('submit');
+	};
+
+	const handleReset = () => {
+		setFontFamily(defaultArticleState.fontFamilyOption);
+		setFontSize(defaultArticleState.fontSizeOption);
+		setFontColor(defaultArticleState.fontColor);
+		setBackgroundColor(defaultArticleState.backgroundColor);
+		setContentWidth(defaultArticleState.contentWidth);
+	};
+
 	return (
 		<main
 			className={clsx(styles.main)}
@@ -25,7 +56,45 @@ const App = () => {
 					'--bg-color': defaultArticleState.backgroundColor.value,
 				} as CSSProperties
 			}>
-			<ArticleParamsForm />
+			<ArticleParamsForm
+				opened={true}
+				onSubmit={handleFormSubmit}
+				onReset={handleReset}
+				fields={[
+					<Select
+						selected={fontFamily}
+						onChange={setFontFamily}
+						options={fontFamilyOptions}
+						title='шрифт'
+					/>,
+					<RadioGroup
+						selected={fontSize}
+						name='radio'
+						onChange={setFontSize}
+						options={fontSizeOptions}
+						title='размер шрифта'
+					/>,
+					<Select
+						selected={fontColor}
+						onChange={setFontColor}
+						options={fontColors}
+						title='цвет шрифта'
+					/>,
+					<Separator />,
+					<Select
+						selected={backgroundColor}
+						onChange={setBackgroundColor}
+						options={backgroundColors}
+						title='цвет фона'
+					/>,
+					<Select
+						selected={contentWidth}
+						onChange={setContentWidth}
+						options={contentWidthArr}
+						title='ширина контента'
+					/>
+				]}
+			/>
 			<Article />
 		</main>
 	);
